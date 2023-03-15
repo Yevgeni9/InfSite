85440937c95d2485bf631780eb999f3dc4d1cbc3
Yevgeni9 <127859035+Yevgeni9@users.noreply.github.com>
2023-03-15 17:34:14 +0100

    Create Site.js

---
+++ Site.js
@@ -0,0 +1,25 @@
+const blob = document.getElementById("blob");
+
+document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    blob.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, {duration: 5000, fill: "forwards" });
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
